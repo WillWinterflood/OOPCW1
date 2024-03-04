e59d8c214b147a645760e390483011ebfd9f83fa
WillWinterflood <146846548+WillWinterflood@users.noreply.github.com>
2024-03-04 12:57:45 +0000

    14/26

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -8,18 +8,25 @@
 import java.time.ZonedDateTime;
 import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.time.temporal.ChronoUnit;
 
 public class Track {
-  private String PointData;
+  
+  private List<Point> points;
 
   // TODO: Create a stub for the constructor
   public Track()  {
-    PointData = "";
-  
 
+    points = new ArrayList<>();
+    
   }
 
   public Track (String filename) {
+    this();
     readFile(filename);
 
 
@@ -27,30 +34,36 @@ public class Track {
 
   // TODO: Create a stub for readFile()
   public void readFile(String filename) {
-    Scanner input = new Scanner(filename);
+    try {
+      File file = new File(filename);
+  
+      Scanner input = new Scanner(file);
     
-    while (input.hasNextLine()) {
-      String line = input.nextLine();
-      String[] variables = line.split(",");
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        String[] variables = line.split(",");
 
-      ZonedDateTime t = ZonedDateTime.parse(variables[0]);
-      double lon = Double.valueOf(variables[1]);
-      double lat = Double.valueOf(variables[2]);
-      double elev = Double.valueOf(variables[3]);
+        ZonedDateTime t = ZonedDateTime.parse(variables[0]);
+        double lon = Double.valueOf(variables[1]);
+        double lat = Double.valueOf(variables[2]);
+        double elev = Double.valueOf(variables[3]);
 
-      Point temp = new Point(t, lon, lat, elev);
-      add(temp);
+        Point temp = new Point(t, lon, lat, elev);
+        add(temp);
 
-      System.out.printf("%d (%.5f, %.5f), %.5fn",t , lon, lat, elev);
+        System.out.printf("%s, (%.5f, %.5f), %.5fn",t , lon, lat, elev);
+      }
+
+      input.close();
     }
-
-    input.close();
-
+    catch (FileNotFoundException e) {
+      throw new GPSException("Error loading file.");
+    }
   }
 
   // TODO: Create a stub for add()
   public void add(Point point) {
-
+    points.add(point);
   }
   
 
@@ -63,23 +76,51 @@ public class Track {
 
   // TODO: Create a stub for size()
   public int size() {
-    return 0;
+    return points.size();
 
   }
 
   // TODO: Create a stub for lowestPoint()
   public Point lowestPoint() {
-    return null;
+    Point lowestP = points.get(0);
+
+
+    if (points.size() == 0) {
+      throw new GPSException("Track is empty.");
+    }
+
+    else {
+      for (int i = 1; i < points.size(); i++) {
+        Point currentP = points.get(i);
+
+        if (currentP.getElevation() < lowestP.getElevation()) {
+          lowestP = currentP;
+        }
+      } 
+    }
+    return lowestP;
   }
 
   // TODO: Create a stub for highestPoint()
   public Point highestPoint() {
-    return null;
+    Point highestP = points.get(0);
+
+    for (int x = 1; x < points.size(); x++) {
+      Point currentHP = points.get(x);
+
+      if (currentHP.getElevation() > highestP.getElevation()) {
+        highestP = currentHP;
+      }
+    }
+    if (points.size() == 0) {
+      throw new GPSException("Track is empty.");
+    }
+    return highestP;
   }
 
   // TODO: Create a stub for totalDistance()
   public double totalDistance() {
-    return 0;
+   return 0; 
   }
 
   // TODO: Create a stub for averageSpeed()
